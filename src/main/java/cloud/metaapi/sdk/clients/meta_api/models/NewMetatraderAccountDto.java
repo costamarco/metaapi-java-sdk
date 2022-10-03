28d156296a13baf2bd532644763cc8a8714e5c77
costamarco <marco.costa@me.com>
2022-10-03 21:38:20 +0200

    region added

--- src/main/java/cloud/metaapi/sdk/clients/meta_api/models/NewMetatraderAccountDto.java
+++ src/main/java/cloud/metaapi/sdk/clients/meta_api/models/NewMetatraderAccountDto.java
@@ -15,9 +15,12 @@ public class NewMetatraderAccountDto {
    */
   public String name;
   /**
-   * Account type, can be cloud, cloud-g1, cloud-g2 or self-hosted. cloud-g2 and cloud are aliases. 
-   * When you create MT5 cloud account the type is automatically converted to cloud-g1 because MT5 G2 support
-   * is still experimental. You can still create MT5 G2 account by setting type to cloud-g2.
+   * Account type, can be cloud, cloud-g1, cloud-g2 or self-hosted. cloud-g2 and
+   * cloud are aliases.
+   * When you create MT5 cloud account the type is automatically converted to
+   * cloud-g1 because MT5 G2 support
+   * is still experimental. You can still create MT5 G2 account by setting type to
+   * cloud-g2.
    */
   public String type;
   /**
@@ -25,8 +28,10 @@ public class NewMetatraderAccountDto {
    */
   public String login;
   /**
-   * MetaTrader account password. The password can be either investor password for read-only
-   * access or master password to enable trading features. Required for cloud account
+   * MetaTrader account password. The password can be either investor password for
+   * read-only
+   * access or master password to enable trading features. Required for cloud
+   * account
    */
   public String password;
   /**
@@ -42,7 +47,8 @@ public class NewMetatraderAccountDto {
    */
   public String provisioningProfileId;
   /**
-   * Application name to connect the account to. Currently allowed values are MetaApi and AgiliumTrade
+   * Application name to connect the account to. Currently allowed values are
+   * MetaApi and AgiliumTrade
    */
   public String application;
   /**
@@ -50,12 +56,15 @@ public class NewMetatraderAccountDto {
    */
   public long magic;
   /**
-   * Flag indicating if trades should be placed as manual trades, or {@code null}. Default is false.
+   * Flag indicating if trades should be placed as manual trades, or {@code null}.
+   * Default is false.
    */
   public Boolean manualTrades;
   /**
-   * Quote streaming interval in seconds, or {@code null}. Set to 0 in order to receive quotes on each tick.
-   * Default value is 2.5 seconds. Intervals less than 2.5 seconds are supported only for G2
+   * Quote streaming interval in seconds, or {@code null}. Set to 0 in order to
+   * receive quotes on each tick.
+   * Default value is 2.5 seconds. Intervals less than 2.5 seconds are supported
+   * only for G2
    */
   public Double quoteStreamingIntervalInSeconds;
   /**
@@ -71,13 +80,17 @@ public class NewMetatraderAccountDto {
    */
   public Map<String, Object> metadata;
   /**
-   * Used to increase the reliability of the account. Allowed values are regular and high. Default is regular
+   * Used to increase the reliability of the account. Allowed values are regular
+   * and high. Default is regular
    */
   public String reliability;
   /**
-   * 3-character ISO currency code of the account base currency. Default value is USD.
-   * The setting is to be used for copy trading accounts which use national currencies
-   * only, such as some Brazilian brokers. You should not alter this setting unless you
+   * 3-character ISO currency code of the account base currency. Default value is
+   * USD.
+   * The setting is to be used for copy trading accounts which use national
+   * currencies
+   * only, such as some Brazilian brokers. You should not alter this setting
+   * unless you
    * understand what you are doing.
    */
   public String baseCurrency;
@@ -86,10 +99,16 @@ public class NewMetatraderAccountDto {
    */
   public List<CopyFactoryRole> copyFactoryRoles;
   /**
-   * Number of resource slots to allocate to account, or {@code null}. Allocating extra resource slots
-   * results in better account performance under load which is useful for some applications. E.g. if you have many
-   * accounts copying the same strategy via CopyFactory API, then you can increase resourceSlots to get a lower trade
-   * copying latency. Please note that allocating extra resource slots is a paid option. Default is 1
+   * Number of resource slots to allocate to account, or {@code null}. Allocating
+   * extra resource slots
+   * results in better account performance under load which is useful for some
+   * applications. E.g. if you have many
+   * accounts copying the same strategy via CopyFactory API, then you can increase
+   * resourceSlots to get a lower trade
+   * copying latency. Please note that allocating extra resource slots is a paid
+   * option. Default is 1
    */
   public Integer resourceSlots;
+
+  public String region;
 }
